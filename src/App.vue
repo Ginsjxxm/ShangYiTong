fac11ea7d080f950dbc189a7e7d0bb32acdf5ce0
Ginsjxxm <3288449152@qq.com>
2024-04-07 17:39:07 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<<template>
+  <div class="container">
+    <HospitalTop/>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <HospitalBottom/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .content{
+      margin-top: 70px;
+      width: 1200px;
+      min-height: 700px;
+    }
+    .bottom{
+      width: 100%;
+      height: 50px;
+      
+    }
+  }
+</style>
+
+
